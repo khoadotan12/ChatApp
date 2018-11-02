5f84f04c1133bf73e8fa6bac482c5746bd3ff6bc
root <1512252@student.hcmus.edu.vn>
2018-11-02 10:28:26 +0700

    add css online list

--- source/src/Home.js
+++ source/src/Home.js
@@ -2,17 +2,13 @@ import React, { Component } from 'react'
 import './App.css'
 import logo from './logo.svg';
 import Navbar from './Navbar';
+import OnlineList from './OnlineList';
 class Home extends Component {
   render() {
     return (
-      <div className='App'>
-        <div className='App-header'>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Welcome
-          </p>
-          <Navbar />
-        </div>
+      <div className="container clearfix">
+        <Navbar />
+        <OnlineList />
       </div>
     )
   }
